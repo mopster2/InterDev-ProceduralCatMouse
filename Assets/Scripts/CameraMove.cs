4f3bc30fc036617cc065010ff6b35fcce558503b
Erik <emm615@nyu.edu>
2016-02-25 16:42:44 -0500

    movement added to character controller, have looking up and down on mouse wheel.

---
+++ Assets/Scripts/CameraMove.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraMove : MonoBehaviour {
+
+    public float aimSpeed;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        Transform getTransform = GetComponent<Transform>();
+
+        float aimDirrection = -Input.GetAxis("Mouse ScrollWheel");
+        float cameraRotation = aimSpeed * Time.deltaTime * aimDirrection;
+
+        transform.Rotate(cameraRotation, 0f, 0f);
+
+    }
+}
--- Assets/Scripts/PlayerControll.cs
+++ Assets/Scripts/PlayerControll.cs
@@ -3,13 +3,39 @@ using System.Collections;
 
 public class PlayerControll : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
+    public float moveSpeed = 0.06f;
+    public float turnSpeed = 90f;
+
+    // Use this for initialization
+    void Start () {
 	
 	}
 	
 	// Update is called once per frame
 	void Update () {
-	
-	}
+
+        float horizontal = Input.GetAxis("Horizontal");
+        float vertical = Input.GetAxis("Vertical");
+
+        CharacterController contoller = GetComponent<CharacterController>();
+        Vector3 movement = transform.forward * moveSpeed * vertical;
+
+        if (Input.GetKey(KeyCode.W))
+        {
+            contoller.Move((movement+Physics.gravity)*Time.deltaTime);
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            contoller.Move((movement + Physics.gravity) * Time.deltaTime);
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            transform.Rotate(0f, horizontal * turnSpeed * Time.deltaTime, 0f);
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            transform.Rotate(0f, horizontal*turnSpeed * Time.deltaTime, 0f);
+        }
+
+    }
 }
